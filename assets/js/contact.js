5e9e9bed4cfb4c1a50b09934d3775d1def504da5
Russellg <103833866+RussellGN@users.noreply.github.com>
2022-08-23 12:56:31 +0200

    changed abouts us section on homepage

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,4 +1,10 @@
-let textArea = document.getElementById("message-textarea");
+import Botpoison from "@botpoison/browser";
+
+const botpoison = new Botpoison({
+	publicKey: "pk_1197335d-48ea-4d06-86f9-10be4aaccf44",
+});
+
+const textArea = document.getElementById("message-textarea");
 textArea.onfocus = function () {
 	if (this.value == "Your Message") {
 		this.value = "";
